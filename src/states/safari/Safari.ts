cb6f667f026523084c4cb60434ea0994eb228af4
alex <alex@3axis.io>
2020-07-26 18:35:51 +0200

    save start position

--- src/states/safari/Safari.ts
+++ src/states/safari/Safari.ts
@@ -1,18 +1,22 @@
 import Scene from "../../skeleton/Scene";
 import Tween from "../../skeleton/Tween";
 import Label from "../../skeleton/gui/Label";
+import Tile from "../../skeleton/gui/Tile";
 
 export default class Safari extends Scene {
     private _tw1?: Tween;
     private _tw2?: Tween;
     private _play: boolean;
     private _speed: number;
+    private _bgPosition: number;
 
     /**
      * Запуск игры
      */
     start(): void {
         super.start();
+
+        this._bgPosition = 0;
 
         // время в милисекундах за которое проходит экран целиком
         this._speed = 15000;
@@ -28,7 +32,7 @@ export default class Safari extends Scene {
         // движение экрана
         this._tw1 = this.addTween()
             .addControl("tile", "game")
-            .do({tilePositionX: [0, -1280]})
+            .do({tilePositionX: [this._bgPosition + 0, this._bgPosition - 1280]})
             .start(this._speed, undefined, -1);
 
         // покачивание машины
@@ -45,6 +49,7 @@ export default class Safari extends Scene {
     stopMove(): void {
         if (this._tw1) this._tw1.stop();
         if (this._tw2) this._tw2.stop();
+        this._bgPosition = (<Tile>this.getControl("tile", "game")).tilePositionX;
         this._play = false;
     }
 
